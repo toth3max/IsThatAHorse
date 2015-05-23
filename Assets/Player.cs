e76dd374f261e21d86470885a156bee8d6e1f171
Richard Baxter <richard.jonathan.baxter@gmail.com>
2015-05-23 12:05:29 +0200

    YAAAAAAAAY!

---
+++ Assets/Player.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public Transform leftFoot;
+	public Transform rightFoot;
+
+	public LayerMask groundLayers;
+
+	public float jumpVelocity = 15;
+	public float gravity = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public bool onGround { get; private set;}
+
+	public int hackyFrameWait = 5;
+
+	float yVelocity = 0;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (hackyFrameWait > 0)
+		{
+			hackyFrameWait -= 1;
+			return;
+		}
+
+		transform.rotation = Quaternion.Euler(0, RotateCamera.instance.yDirection, 0);
+
+		RotateCamera.instance.Height = transform.position.y;
+		
+		Vector3 cameraPosition = RotateCamera.instance.centerOculus.position;
+
+		bool hitGround = false;
+
+		int groundIntervals = 5;
+		for (int i = 0 ; i < groundIntervals ; i++)
+		{
+			Vector3 feetGround = Vector3.Lerp(leftFoot.position, rightFoot.position, (float)i/(groundIntervals-1f));
+
+			Vector3 direction = feetGround - cameraPosition;
+
+			Debug.DrawRay(cameraPosition, direction*2, Color.red);
+
+			RaycastHit [] hits = Physics.RaycastAll(cameraPosition, direction, direction.magnitude*2, groundLayers.value);
+
+			if (hits.Length > 0)
+			{
+				hitGround = true;
+				break ;// HACK
+			}
+			Debug.Log (i+": "+hits.Length);
+		}
+
+		onGround = hitGround;
+		if (hitGround)
+		{
+			// DESTROY GRAVITY
+			yVelocity = 0;
+
+			// jump
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				yVelocity = jumpVelocity;
+			}
+		}
+		else
+		{
+			// apply gravity	
+		    yVelocity -= gravity*Time.deltaTime;
+		}
+
+		transform.position += Vector3.up*yVelocity*Time.deltaTime;
+
+	}
+}
+
+
+
+
+
+
---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour 
+{
+	public static RotateCamera instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+//	public float distance = 60;
+
+	public Transform centerOculus;
+	public Transform trackingCenter;
+
+	
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	public float yDirection { get; private set;}
+	public float Height 
+	{ 
+		get
+		{
+			return transform.position.y;
+		}
+		set
+		{
+			transform.position = new Vector3(transform.position.x, value, transform.position.z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 flatForward = centerOculus.transform.localRotation.eulerAngles;
+
+		yDirection = flatForward.y;
+
+		Debug.DrawRay(transform.position, Quaternion.AngleAxis(flatForward.y, Vector3.up)*Vector3.forward * 100, Color.red);
+
+		transform.localRotation = Quaternion.Euler(0, flatForward.y, 0);
+
+		trackingCenter.localRotation = Quaternion.Euler(0, -flatForward.y, 0);
+	}
+}
