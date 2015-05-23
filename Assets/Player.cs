9e26c1a940ce00d57c14c35417e8e865072d3556
Richard Baxter <richard.jonathan.baxter@gmail.com>
2015-05-23 15:04:44 +0200

    player snaps to object as it lands on it now

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour 
@@ -37,6 +37,10 @@ public class Player : MonoBehaviour
 		
 		Vector3 cameraPosition = RotateCamera.instance.centerOculus.position;
 
+		Vector3 closestHitPoint = Vector3.zero;
+		Vector3 oppositeSide = Vector3.zero;
+		GameObject closestObject = null;
+
 		bool hitGround = false;
 
 		int groundIntervals = 5;
@@ -46,19 +50,28 @@ public class Player : MonoBehaviour
 
 			Vector3 direction = feetGround - cameraPosition;
 
-			Debug.DrawRay(cameraPosition, direction*2, Color.red);
 
 			RaycastHit [] hits = Physics.RaycastAll(cameraPosition, direction, direction.magnitude*2, groundLayers.value);
 
+			Debug.DrawRay(cameraPosition, direction*2, Color.red);
+
 			if (hits.Length > 0)
 			{
+				float distance = Vector3.Distance(hits[0].point, cameraPosition);
+				float closestDistance = Vector3.Distance(closestHitPoint, cameraPosition);
+
+				if (distance < closestDistance || closestObject == null)
+				{
+					closestHitPoint = hits[0].point;
+					closestObject = hits[0].collider.gameObject;
+					oppositeSide = cameraPosition + direction*2;
+				}
 				hitGround = true;
-				break ;// HACK
 			}
-//			Debug.Log (i+": "+hits.Length);
 		}
 
 		onGround = hitGround;
+		bool wasGoingDown = yVelocity < 0;
 		if (hitGround)
 		{
 			// DESTROY GRAVITY
@@ -90,7 +103,7 @@ public class Player : MonoBehaviour
 		}
 //		Debug.Log(leftMove);
 
-		
+		// left right controls
 		Vector3 centerFoot = Vector3.Lerp(leftFoot.position, rightFoot.position, 0.5f);
 		
 		Vector3 centerDirection = centerFoot - cameraPosition;
@@ -99,6 +112,51 @@ public class Player : MonoBehaviour
 
 		transform.position -= leftMove*rightVector*moveSpeed*Time.deltaTime;
 
+		
+		// teleport in 'z' direction toclosest object
+		// only teleport when moving downwards and not grounded
+		if (onGround && wasGoingDown)
+		{
+			if (closestObject != null)
+			{
+				Vector3 rayDirection = oppositeSide - cameraPosition;
+				RaycastHit [] oppositeHits = Physics.RaycastAll(oppositeSide, -rayDirection, rayDirection.magnitude, groundLayers);
+				RaycastHit [] forwardHits = Physics.RaycastAll(cameraPosition, rayDirection, rayDirection.magnitude, groundLayers);
+
+
+				int oppositeHitIndex = System.Array.FindIndex(oppositeHits, (h) => h.collider.gameObject == closestObject);
+				int forwardHitIndex = System.Array.FindIndex(forwardHits, (h) => h.collider.gameObject == closestObject);
+				
+				if (oppositeHitIndex == -1)
+					Debug.LogError("Did not raycast a hit in the opposite direciton, IMPOSSIBRU!");
+
+				if (forwardHitIndex == -1)
+					Debug.LogError("Did not raycast a hit in the foraward direciton, IMPOSSIBRU!");
+				
+				Vector3 farHit = oppositeHits[oppositeHitIndex].point;
+				Vector3 closeHit = forwardHits[forwardHitIndex].point;
+
+				
+				float closeHitDistance = Vector3.Distance(closeHit, cameraPosition);
+				float farHitDistance   = Vector3.Distance(farHit, cameraPosition);
+
+				float playerDistance   = Vector3.Distance(transform.position, cameraPosition);
+
+				Debug.Log (closeHitDistance +" : "+playerDistance+" : "+farHitDistance);
+				float targetPlayerPosition = Mathf.Clamp(playerDistance, closeHitDistance, farHitDistance);
+
+				// TODO target distances is WRONG!
+
+				Debug.Log (closestObject);
+				Vector3 playerSphericalPosition = centerDirection.normalized;
+				transform.position = cameraPosition + playerSphericalPosition * targetPlayerPosition;
+				
+				Debug.DrawRay(cameraPosition, transform.position, Color.red);
+			}
+		}
+
+
+		// adjust camera position to player position
 		RotateCamera.instance.transform.position = transform.position;
 		RotateCamera.instance.Height = transform.position.y;
 	}
